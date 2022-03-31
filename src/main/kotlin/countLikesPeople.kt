4777757e99138c7301d1f16768f9ca63f041a151
Graf <98654937+MelikovGraf@users.noreply.github.com>
2022-03-31 09:54:30 +0300

    Update countLikesPeople.kt

--- src/main/kotlin/countLikesPeople.kt
+++ src/main/kotlin/countLikesPeople.kt
@@ -1,7 +1,7 @@
 fun main() {
     while (true) {
         print("Введите число лайков: ")
-        val count = readln()?.toUInt(); if (count != 11u && count % 10u == 1u) println("Нравится $count человеку")
+        val count = readln().toUInt(); if (count != 11u && count != 111u && count != 1111u && count % 10u == 1u) println("Нравится $count человеку")
         else println("Нравится $count людям")
     }
 }
