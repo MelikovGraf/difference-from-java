1cd5788186d3ce41e6bdb10b6c12aa75fa18d789
Graf <98654937+MelikovGraf@users.noreply.github.com>
2022-03-31 10:49:54 +0300

    Update meloman.kt

--- src/main/kotlin/meloman.kt
+++ src/main/kotlin/meloman.kt
@@ -1,38 +1,32 @@
 const val THOUSAND = 1000
 const val TEN_THOUSAND = 10000
 const val FIRST_COUPON = 100
-const val VALUE_TO_CALCULATE_PROCENT = 99
-const val SECOND_SECOND_VALUE_TO_CALCULATE_PROCENT = 20
+const val THIRD_COUPON = .99
+const val SECOND_COUPON = .95
 
 fun main() {
 
     while (true) {
         print("Введите сумму покупки: ")
-        var amount = readln()?.toDouble()
+        var amount = readln().toDouble()
         print("Введите цифру 1, если вы в первый раз: ")
-        val user = readln()?.toInt()
-        val amountWithDiscount = (pay(amount) / 100) * VALUE_TO_CALCULATE_PROCENT
-        val amountWithDiscountCent = (payCent(amount) / 100) * VALUE_TO_CALCULATE_PROCENT
+        val user = readln().toInt()
         if (user == 1)
-            println("Сумма к оплате с учетом скидки: ${pay(amount)} рублей ${payCent(amount).toInt()} копеек")
+            println("Сумма к оплате с учетом скидки: ${pay(amount)} рублей ${payCent(amount)} копеек")
         else
-            println("Сумма к оплате с учетом скидки: $amountWithDiscount рублей ${amountWithDiscountCent.toInt()} копеек")
+            println("Сумма к оплате с учетом скидки: ${pay(amount)*THIRD_COUPON} рублей ${payCent(amount)*THIRD_COUPON} копеек")
 
     }
 }
 
-fun pay(amount: Double): Int {
+fun pay(amount: Double): Double {
     return if (amount > TEN_THOUSAND)
-        (amount - (amount / SECOND_SECOND_VALUE_TO_CALCULATE_PROCENT)).toInt()
+        (amount * SECOND_COUPON)
     else if (amount > THOUSAND)
-        (amount - FIRST_COUPON).toInt()
-    else amount.toInt()
+        (amount - FIRST_COUPON)
+    else amount
 }
 
-fun payCent(amount: Double): Double {
-    return if (amount > TEN_THOUSAND)
-        (((amount - (amount / SECOND_SECOND_VALUE_TO_CALCULATE_PROCENT)) - (amount - (amount / SECOND_SECOND_VALUE_TO_CALCULATE_PROCENT)).toInt()) * 100)
-    else if (amount > THOUSAND)
-        (((amount - FIRST_COUPON) - (amount - FIRST_COUPON).toInt()) * 100)
-    else ((amount - (amount).toInt()) * 100)
+fun payCent(amount: Double): Int {
+    return ((amount - amount.toInt())*100).toInt()
 }
